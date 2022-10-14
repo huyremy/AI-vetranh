f22d3f6a9fd316df0a0267aad61574fd8ba00019
Nguyễn Quang Huy <huynq@isi.com.vn>
2022-10-14 17:06:00 +0700

    Update draw.py

--- draw.py
+++ draw.py
@@ -1,6 +1,6 @@
 from diffusers import StableDiffusionPipeline
 import torch
-model_id = "path-to-your-trained-model" # đường dẫn đến model
+model_id = "/path/to/trained/model" # đường dẫn đến model
 pipe = StableDiffusionPipeline.from_pretrained(model_id, torch_dtype=torch.float16).to("cuda")
 prompt = "A dog eating bones" # thích vẽ cái gì thì điền vào đây
 image = pipe(prompt, num_inference_steps=50, guidance_scale=7.5).images[0]
